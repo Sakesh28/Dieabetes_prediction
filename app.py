3a55fc26a87ad122d51e431d3a8b7119eb0046ba
Sakesh28 <sakesh28sk@gmail.com>
2025-02-20 13:01:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score # type: ignore
 st.set_page_config(page_title="Diabetes Prediction", layout="wide", page_icon="🧑‍⚕")
 
 # Load the saved diabetes model
-diabetes_model_path = r"C:\Users\VL-164\Desktop\analysis_CS103\diabetes_model.sav"
+diabetes_model_path = r"diabetes_model.sav"
 diabetes_model = pkl.load(open(diabetes_model_path, "rb"))
 
 # Page title
@@ -76,7 +76,7 @@ st.write(diab_diagnosis)
 
 if st.button("show model accuracy"):
     
-    test_data =  pd.read_csv(r"C:\Users\VL-164\Desktop\analysis_CS103\diabetes.csv")
+    test_data =  pd.read_csv(r"diabetes.csv")
 
     X = test_data.drop("Outcome", axis=1)
     y = test_data["Outcome"]
